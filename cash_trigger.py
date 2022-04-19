cb91fe25b8d10fbfbda0be231ddcad1562e8bf0e
Ian Kaplan <ian@possiblefinance.com>
2022-04-19 18:23:37 -0400

    Added code for moving average.

---
+++ cash_trigger.py
@@ -0,0 +1,189 @@
+
+from datetime import datetime, timedelta
+from tabulate import tabulate
+from typing import List, Tuple
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import pandas as pd
+from pandas.core.indexes.datetimes import DatetimeIndex
+from dateutil.relativedelta import relativedelta
+import numpy as np
+from pathlib import Path
+import tempfile
+
+plt.style.use('seaborn-whitegrid')
+pd.options.mode.chained_assignment = 'raise'
+
+
+def get_market_data(file_name: str,
+                    data_col: str,
+                    symbols: List,
+                    data_source: str,
+                    start_date: datetime,
+                    end_date: datetime) -> pd.DataFrame:
+    """
+      file_name: the file name in the temp directory that will be used to store the data
+      data_col: the type of data - 'Adj Close', 'Close', 'High', 'Low', 'Open', Volume'
+      symbols: a list of symbols to fetch data for
+      data_source: yahoo, etc...
+      start_date: the start date for the time series
+      end_date: the end data for the time series
+      Returns: a Pandas DataFrame containing the data.
+
+      If a file of market data does not already exist in the temporary directory, fetch it from the
+      data_source.
+    """
+    temp_root: str = tempfile.gettempdir() + '/'
+    file_path: str = temp_root + file_name
+    temp_file_path = Path(file_path)
+    file_size = 0
+    if temp_file_path.exists():
+        file_size = temp_file_path.stat().st_size
+
+    if file_size > 0:
+        close_data = pd.read_csv(file_path, index_col='Date')
+    else:
+        panel_data: pd.DataFrame = data.DataReader(symbols, data_source, start_date, end_date)
+        close_data: pd.DataFrame = panel_data[data_col]
+        close_data.to_csv(file_path)
+    assert len(close_data) > 0, f'Error reading data for {symbols}'
+    return close_data
+
+window_size = 200
+
+trading_days = 252
+trading_quarter = trading_days // 4
+
+data_source = 'yahoo'
+
+start_date_str = '2008-03-03'
+start_date: datetime = datetime.fromisoformat(start_date_str)
+# The "current date"
+end_date: datetime = datetime.today() - timedelta(days=1)
+
+
+def convert_date(some_date):
+    if type(some_date) == str:
+        some_date = datetime.fromisoformat(some_date)
+    elif type(some_date) == np.datetime64:
+        ts = (some_date - np.datetime64('1970-01-01T00:00')) / np.timedelta64(1, 's')
+        some_date = datetime.utcfromtimestamp(ts)
+    return some_date
+
+
+def findDateIndex(date_index: DatetimeIndex, search_date: datetime) -> int:
+    '''
+    In a DatetimeIndex, find the index of the date that is nearest to search_date.
+    This date will either be equal to search_date or the next date that is less than
+    search_date
+    '''
+    index: int = -1
+    i = 0
+    search_date = convert_date(search_date)
+    date_t = datetime.today()
+    for i in range(0, len(date_index)):
+        date_t = convert_date(date_index[i])
+        if date_t >= search_date:
+            break
+    if date_t > search_date:
+        index = i - 1
+    else:
+        index = i
+    return index
+
+
+class SpyData:
+    spy_close_file = 'spy_close'
+    spy_etf = "SPY"
+    spy_close: pd.DataFrame
+    date_index: DatetimeIndex
+
+    def __init__(self, start_date: datetime, end_date: datetime):
+        spy_start: datetime = start_date - timedelta(days=365)
+        self.spy_close = get_market_data(file_name=self.spy_close_file,
+                                         data_col='Close',
+                                         symbols=self.spy_etf,
+                                         data_source=data_source,
+                                         start_date=spy_start,
+                                         end_date=end_date)
+        self.date_index = self.spy_close.index
+
+    def spy_close(self, start_date: datetime, end_date: datetime) -> pd.DataFrame:
+        """
+        Return a section of SPY close prices from start_date to end_date
+        """
+        start_ix = findDateIndex(date_index=self.date_index, search_date=start_date)
+        end_ix = findDateIndex(date_index=self.date_index, search_date=end_date)
+        spy_close_df = pd.DataFrame()
+        assert start_ix >= 0 and end_ix >= 0
+        spy_close_df = self.spy_close[:][start_ix:end_ix+1].copy()
+        return spy_close_df
+
+    def avg(self, day: datetime, window: int = window_size) -> float:
+        """
+        :param day: the end date for the window
+        :param window: the size of the window extending back from day
+        :return: the average for the SPY close prices in the window
+        """
+        _average: float = -1.0
+        end_ix = findDateIndex(date_index=self.date_index, search_date=day)
+        start_ix = end_ix - (window -1)
+        assert start_ix >= 0 and end_ix >= 0
+        _average = self.spy_close.values[start_ix:end_ix+1].mean()
+        return _average
+
+    def running_sum(self, values: np.array, start_ix: int, end_ix: int, win_size: int) -> np.array:
+            sum_l: list = []
+            win_start_ix = start_ix - (win_size - 1)
+            win_end_ix = start_ix
+            sum = values[win_start_ix:win_end_ix + 1].sum()
+            win_end_ix = win_end_ix + 1
+            sum_l.append(sum)
+            while win_end_ix <= end_ix:
+                win_start_ix = win_end_ix - win_size
+                start_val = values[win_start_ix]
+                end_val = values[win_end_ix]
+                sum = (sum - start_val) + end_val
+                sum_l.append(sum)
+                win_end_ix = win_end_ix + 1
+            sum_a = np.array(sum_l)
+            return sum_a
+
+    def moving_avg(self, start_date: datetime, end_date: datetime, window: int = window_size) -> pd.DataFrame:
+        """
+        Compute a moving average series
+        :param start_date: the start date for the moving average series
+        :param end_date: the end date for the moving average series
+        :param window: the window size that extends, initially, back from start_date
+        :return: a moving average series as a DataFrame. The date index will be the same
+                 as the SPY time DataFrame between start_date and end_date
+        """
+        start_ix = findDateIndex(date_index=self.date_index, search_date=start_date)
+        end_ix = findDateIndex(date_index=self.date_index, search_date=end_date)
+        win_start_ix = start_ix - (window - 1)
+        assert start_ix >= 0 and end_ix >= 0 and win_start_ix >= 0
+        sum_l: list = []
+        spy_values = self.spy_close.values
+        sum = spy_values[win_start_ix:start_ix + 1].sum()
+        win_start_ix = win_start_ix + 1
+        start_ix = start_ix + 1
+        while start_ix <= end_ix:
+            sum_l.append(sum)
+            start_val = spy_values[win_start_ix]
+            end_val = spy_values[start_ix]
+            sum = (sum - start_val) + end_val
+            win_start_ix = win_start_ix + 1
+            start_ix = start_ix + 1
+
+
+
+spy_data = SpyData(start_date, end_date)
+spy_start_avg = spy_data.avg(start_date)
+
+d_end = 13
+d = np.array(range(1, d_end))
+sum_a = spy_data.running_sum(values=d, start_ix=3, end_ix=(len(d) - 1), win_size=4)
+print(d)
+print(sum_a)
+
+print("Hi there")
