4185e6183e65c1fd9857841eda11bb17512b6088
Ian Kaplan <ian@possiblefinance.com>
2022-04-21 15:11:24 -0400

    Checkin before changes.

--- cash_trigger.py
+++ cash_trigger.py
@@ -1,5 +1,7 @@
 
 from datetime import datetime, timedelta
+from enum import Enum
+
 from tabulate import tabulate
 from typing import List, Tuple
 from pandas_datareader import data
@@ -43,6 +45,10 @@ def get_market_data(file_name: str,
     if file_size > 0:
         close_data = pd.read_csv(file_path, index_col='Date')
     else:
+        if type(symbols) == str:
+            t = list()
+            t.append(symbols)
+            symbols = t
         panel_data: pd.DataFrame = data.DataReader(symbols, data_source, start_date, end_date)
         close_data: pd.DataFrame = panel_data[data_col]
         close_data.to_csv(file_path)
@@ -90,6 +96,11 @@ def findDateIndex(date_index: DatetimeIndex, search_date: datetime) -> int:
     else:
         index = i
     return index
+
+
+class RiskState(Enum):
+    RISK_OFF = 0
+    RISK_ON = 1
 
 
 class SpyData:
@@ -179,11 +190,224 @@ class SpyData:
         avg_df.columns = [f'{self.spy_etf} {window_size}-day avg']
         return avg_df
 
+    def risk_state(self, day: datetime) -> RiskState:
+        spy_avg = self.avg(day)
+        day_ix = findDateIndex(date_index=self.date_index, search_date=day)
+        spy_val = float(self.spy_close.values[day_ix:day_ix+1])
+        # only use full dollar price changes
+        spy_avg = round(spy_avg, 0)
+        spy_val = round(spy_val, 0)
+        state: RiskState = RiskState.RISK_ON if spy_val > spy_avg else RiskState.RISK_OFF
+        return state
+
 
 spy_data = SpyData(start_date, end_date)
 spy_close = spy_data.close_data(start_date, end_date)
 spy_moving_avg = spy_data.moving_avg(start_date, end_date)
 plot_df = pd.concat([spy_close, spy_moving_avg], axis=1)
 plot_df.plot(grid=True, title=f'SPY and 200-day average: {start_date.strftime("%m/%d/%Y")} - {end_date.strftime("%m/%d/%Y")}', figsize=(10,6))
+
+
+def chooseAssetName(start: int, end: int, asset_set: pd.DataFrame) -> str:
+    '''
+    Choose an ETF asset in a particular range of close price values.
+    The function returns the name of the asset with the highest returning
+    asset in the period.
+    '''
+    asset_columns = asset_set.columns
+    asset_name = asset_columns[0]
+    if len(asset_columns) > 1:
+        ret_list = []
+        start_date = asset_set.index[start]
+        end_date = asset_set.index[end]
+        for asset in asset_set.columns:
+            ts = asset_set[asset][start:end+1]
+            start_val = ts[0]
+            end_val = ts[-1]
+            r = (end_val/start_val) - 1
+            ret_list.append(r)
+        ret_df = pd.DataFrame(ret_list).transpose()
+        ret_df.columns = asset_set.columns
+        ret_df = round(ret_df, 3)
+        column = ret_df.idxmax(axis=1)[0]
+        asset_name = column
+    return asset_name
+
+
+def find_month_periods(start_date: datetime, end_date:datetime, data: pd.DataFrame) -> pd.DataFrame:
+    start_date = convert_date(start_date)
+    end_date = convert_date(end_date)
+    date_index = data.index
+    start_ix = findDateIndex(date_index, start_date)
+    end_ix = findDateIndex(date_index, end_date)
+    start_l = list()
+    end_l = list()
+    cur_month = start_date.month
+    start_l.append(start_ix)
+    i = 0
+    for i in range(start_ix, end_ix+1):
+        date_i = convert_date(date_index[i])
+        if date_i.month != cur_month:
+            end_l.append(i-1)
+            start_l.append(i)
+            cur_month = date_i.month
+    end_l.append(i)
+    # if there is not a full month period, remove the last period
+    if end_l[-1] - start_l[-1] < 18:
+        end_l.pop()
+        start_l.pop()
+    start_df = pd.DataFrame(start_l)
+    end_df = pd.DataFrame(end_l)
+    start_date_df = pd.DataFrame(date_index[start_l])
+    end_date_df = pd.DataFrame(date_index[end_l])
+    periods_df = pd.concat([start_df, start_date_df, end_df, end_date_df], axis=1)
+    periods_df.columns = ['start_ix', 'start_date', 'end_ix', 'end_date']
+    return periods_df
+
+
+def simple_return(time_series: np.array, period: int = 1) -> List :
+    return list(((time_series[i]/time_series[i-period]) - 1.0 for i in range(period, len(time_series), period)))
+
+
+def return_df(time_series_df: pd.DataFrame) -> pd.DataFrame:
+    r_df: pd.DataFrame = pd.DataFrame()
+    time_series_a: np.array = time_series_df.values
+    return_l = simple_return(time_series_a, 1)
+    r_df = pd.DataFrame(return_l)
+    date_index = time_series_df.index
+    r_df.index = date_index[1:len(date_index)]
+    r_df.columns = time_series_df.columns
+    return r_df
+
+
+def apply_return(start_val: float, return_df: pd.DataFrame) -> np.array:
+    port_a: np.array = np.zeros( return_df.shape[0] + 1)
+    port_a[0] = start_val
+    return_a = return_df.values
+    for i in range(1, len(port_a)):
+        port_a[i] = port_a[i-1] + port_a[i-1] * return_a[i-1]
+    return port_a
+
+
+def portfolio_return(holdings: float,
+                     risk_asset: pd.DataFrame,
+                     bond_asset: pd.DataFrame,
+                     spy_data: SpyData,
+                     start_date: datetime,
+                     end_date: datetime) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    portfolio_a = np.zeros(0)
+    portfolio_val = holdings
+    name_l: List = []
+    date_l: List = []
+    month_periods = find_month_periods(start_date, end_date, risk_asset)
+    for index, period in month_periods.iterrows():
+        start_ix = period['start_ix']
+        end_ix = period['end_ix']
+        start_date: datetime = convert_date(period['start_date'])
+        r_df = pd.DataFrame()
+        asset_name = ''
+        if spy_data.risk_state(start_date) == RiskState.RISK_ON:
+            asset_name: str = chooseAssetName(start_ix, end_ix, risk_asset)
+            risk_close_prices = pd.DataFrame(risk_asset[asset_name][start_ix:end_ix+1])
+            r_df = return_df(risk_close_prices)
+        else: # RISK_OFF - bonds
+            asset_name: str = chooseAssetName(start_ix, end_ix, bond_asset)
+            bond_close_prices = pd.DataFrame(bond_asset[asset_name][start_ix:end_ix+1])
+            r_df = return_df(bond_close_prices)
+        name_l.append(asset_name)
+        date_l.append(period['start_date'])
+        port_month_a = apply_return(portfolio_val, r_df)
+        portfolio_val = port_month_a[-1]
+        portfolio_a = np.append(portfolio_a, port_month_a)
+    portfolio_df = pd.DataFrame(portfolio_a)
+    portfolio_df.columns = ['portfolio']
+    num_rows = month_periods.shape[0]
+    first_row = month_periods[:][0:1]
+    last_row = month_periods[:][num_rows - 1:num_rows]
+    start_ix = first_row['start_ix'].values[0]
+    end_ix = last_row['end_ix'].values[0]
+    date_index = risk_asset.index
+    portfolio_index = date_index[start_ix:end_ix + 1]
+    portfolio_df.index = portfolio_index
+    assets_df = pd.DataFrame(name_l)
+    assets_df.columns = ['assets']
+    assets_df.index = date_l
+    return portfolio_df, assets_df
+
+
+def build_plot_data(holdings: float, portfolio_df: pd.DataFrame, spy_df: pd.DataFrame) -> pd.DataFrame:
+    port_start_date = portfolio_df.index[0]
+    port_start_date = convert_date(port_start_date)
+    port_end_date = portfolio_df.index[-1]
+    port_end_date = convert_date(port_end_date)
+    spy_index = spy_df.index
+    spy_start_ix = findDateIndex(spy_index, port_start_date)
+    spy_end_ix = findDateIndex(spy_index, port_end_date)
+    spy_df = pd.DataFrame(spy_df[:][spy_start_ix:spy_end_ix+1])
+    spy_return = return_df(spy_df)
+    spy_return_a = apply_return(start_val=holdings, return_df=spy_return)
+    spy_port = pd.DataFrame(spy_return_a)
+    spy_port.columns = ['SPY']
+    spy_port.index = spy_df.index
+    plot_df = portfolio_df.copy()
+    plot_df['SPY'] = spy_port
+    return plot_df
+
+
+def collapse_asset_df(asset_df: pd.DataFrame) -> pd.DataFrame:
+    date_l: List = []
+    asset_l: List = []
+    current_date = convert_date(asset_df.index[0])
+    date_l.append(current_date)
+    cur_asset = asset_df.values[0][0]
+    asset_l.append(cur_asset)
+    for index in range(1, asset_df.shape[0]):
+        row = asset_df[:][index:index+1]
+        row_name = row.values[0][0]
+        row_date = convert_date(row.index[0])
+        if row_name != cur_asset:
+            date_l.append(row_date)
+            asset_l.append(row_name)
+            cur_asset = row_name
+    collapse_df = pd.DataFrame(asset_l)
+    collapse_df.index = date_l
+    collapse_df.columns = ['asset']
+    return collapse_df
+
+
+holdings = 100000
+cash_trigger_bonds = ['JNK', 'TLT', 'MUB']
+cash_trigger_bond_file = "cash_trigger_bond_adjclose"
+cash_trigger_bond_adjclose = get_market_data(file_name=cash_trigger_bond_file,
+                                             data_col='Adj Close',
+                                             symbols=cash_trigger_bonds,
+                                             data_source=data_source,
+                                             start_date=start_date,
+                                             end_date=end_date)
+
+spy_portfolio_df, assets_df = portfolio_return(holdings=holdings,
+                                               risk_asset=spy_close,
+                                               bond_asset=cash_trigger_bond_adjclose,
+                                               spy_data=spy_data,
+                                               start_date=start_date,
+                                               end_date=end_date
+                                               )
+
+plot_df = build_plot_data(holdings=holdings, portfolio_df=spy_portfolio_df, spy_df=spy_close)
+
+spy_start_val = float(spy_close[:].values[0])
+spy_only_portfolio, t = portfolio_return(holdings=spy_start_val,
+                                         risk_asset=spy_close,
+                                         bond_asset=spy_close,
+                                         spy_data=spy_data,
+                                         start_date=start_date,
+                                         end_date=end_date)
+
+plot_df = build_plot_data(holdings=spy_start_val, portfolio_df=spy_only_portfolio, spy_df=spy_close)
+
+spy_only_index = spy_only_portfolio.index
+
+
+assets_collapsed = collapse_asset_df(assets_df)
 
 print("Hi there")
