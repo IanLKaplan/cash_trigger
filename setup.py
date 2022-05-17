ec796bd61bb7700dabf3cc5b98e5d898b13a246b
Ian Kaplan <ian@possiblefinance.com>
2022-05-17 10:20:42 -0400

    Some editing changes Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cash_trigger',
+    version='',
+    packages=[''],
+    url='https://github.com/IanLKaplan/cash_trigger',
+    license='Apache II',
+    author='iank',
+    author_email='iank@bearcave.com',
+    description='Cash trigger investment strategy notebook'
+)
